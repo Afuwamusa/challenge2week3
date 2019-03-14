297b43661a74b29739ac3e521d0d12a7676bfae1
unknown <afuwamusa@gmail.com>
2019-03-14 21:03:44 +0300

    adding file

--- account.py
+++ account.py
@@ -1,40 +1,43 @@
 class BankAccount():
-    def __init__(self, account_number, account_name, balance):
+    def __init__(self, account_number, account_name, balance=0.00):
         self.account_number = account_number
         self.account_name = account_name
-        self.balance = 0
+        self.balance = balance
     def open(self):
         self.account_name = input("Enter account name: ")
         self.account_number = input("enter account number: ")
+        self.balance = 0
         print("Hello!" + self.account_name + " " + "Green Bank welcomes you!")
         # print("here is your account number")
-    def deposit(self, deposit):
-        self.balance += deposit
+    def deposit(self, amount):
+        self.balance += amount
         print(input("enter acccount name: "))
         print(input("enter account number: "))
         print(input("enter amount to deposit: "))
         print("you have deposited" + " " + str(deposit) + " " + "thank you for depositing with Green bank")
-    def withdrawal(self,withdraw):
-        self.balance >= withdraw
-        self.balance -= withdraw
+    def withdrawal(self,amount):
+        self.balance >= amount
+        self.balance -= amount
         print(input("enter account name: "))
         print(input("enter account number: "))
         print(input("enter amount to withdraw: "))
         print("You have withdrawed" + " " + str(withdraw))
         
-    def get_balance(self):
+    def get_balance(self, amount):
         self.account_name = input("enter account name: ")
         self.account_number =  input("enter account number: ")
         print("your account balance is:\t" + str(self.balance))
-    def delete_account(self, delete):
-        self.account_name == delete
-        self.account_number == delete
+    def delete_account(self, close):
+        self.account_name == close
+        self.account_number == close
         print("Your account is successfully deleted")
             
         
             
  #class instantiation
-account = BankAccount("account number", "account name", "balance" )
+account = BankAccount("ac23663455", "dodo" )
+account.deposit(200000)
+account.withdraw(100000)
 print("Welcome to Green bank!\nPlease select on one of the options below:")
 print("1. open an account")
 print("2.Deposit money")
